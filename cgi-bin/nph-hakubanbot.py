c6fb1c65822b0e372d037a5ff8393a64f564027d
KIHARA Hideto <deton@m1.interq.or.jp>
2015-03-07 19:27:51 +0900

    書く処理が長時間かかるときに途中でkillされないようstdoutをclose

--- cgi-bin/nph-hakubanbot.py
+++ cgi-bin/nph-hakubanbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import cgi
+import os, sys
 import kst2gcode, gcode2mcu
 #import cgitb
 #cgitb.enable()
@@ -7,6 +8,15 @@ import kst2gcode, gcode2mcu
 print "HTTP/1.0 200 OK"
 print "Content-Type: text/plain"
 print
+
+def drawtext(text, size, xypos):
+    # close stdout to avoid being killed while drawing.
+    sys.stdout.flush()
+    os.close(sys.stdout.fileno())
+    kstfont = kst2gcode.KST2GCode()
+    gcode = kstfont.str2gcode(text, scale, xypos)
+    conn = gcode2mcu.McuConnection()
+    conn.draw_gcodes(["M17"] + gcode + ["M18"])
 
 form = cgi.FieldStorage()
 cmd = form.getfirst("cmd")
@@ -22,10 +32,7 @@ if cmd == "drawtext":
     xypos = None
     if x and y:
         xypos = (float(x), float(y))
-    kstfont = kst2gcode.KST2GCode()
-    gcode = kstfont.str2gcode(text, scale, xypos)
-    conn = gcode2mcu.McuConnection()
-    conn.draw_gcodes(["M17"] + gcode + ["M18"])
+    drawtext(text, size, xypos)
 elif cmd == "init":
     initg = ["M101 T30.0 B-30.0 L-30.0 R30.0 I1 J-1", "D1 L2.8 R2.8", "G92 X0 Y0"]
     conn = gcode2mcu.McuConnection()
