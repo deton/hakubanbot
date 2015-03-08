83c7feda860da0f657b09cbcb01a66d231f3df43
KIHARA Hideto <deton@m1.interq.or.jp>
2015-03-08 12:28:38 +0900

    Use tuple for x,y and width,height

--- cgi-bin/eraseg.py
+++ cgi-bin/eraseg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-def make_erase_gcode(x, y, width, height):
+def make_erase_gcode(xypos, widthheight):
     unit = 5
     unity = unit
     unitx = unit * 2
@@ -8,18 +8,16 @@ def make_erase_gcode(x, y, width, height):
     decx = "G0 X-%d" % unitx
     incy = "G0 Y%d" % unity
     decy = "G0 Y-%d" % unity
-    g = ["G90", "G0 X%d Y%d" % (x,y), "G0 Z90", "G91"]
+    g = ["G90", "G0 X%d Y%d" % xypos, "G0 Z90", "G91"]
     horizup = [incx, incy, decx, incy]
-    horizcnt = (height / (unity*2) + 1)
+    horizcnt = (widthheight[1] / (unity*2) + 1)
     horizdown = [decx, decy, incx, decy]
     block = horizup * horizcnt + ["G0 X%d" % (unitx + unit)] + horizdown * horizcnt + ["G0 X-%d" % unit]
-    return g + block * (width / unitx + 1) + ["G90", "G0 Z55"]
+    return g + block * (widthheight[0] / unitx + 1) + ["G90", "G0 Z55"]
 
 if __name__ == "__main__":
     import sys
-    x = int(sys.argv[1])
-    y = int(sys.argv[2])
-    width = int(sys.argv[3])
-    height = int(sys.argv[4])
-    for g in make_erase_gcode(x, y, width, height):
+    xypos = (int(sys.argv[1]), int(sys.argv[2]))
+    widthheight = (int(sys.argv[3]), int(sys.argv[4]))
+    for g in make_erase_gcode(xypos, widthheight):
         print g
