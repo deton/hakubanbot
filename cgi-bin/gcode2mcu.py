46f12109dac23d33b8a62814fc5db3b1007148a3
KIHARA Hideto <deton@m1.interq.or.jp>
2015-03-08 18:18:04 +0900

    stdoutをcloseしている場合、書かれる文字のサイズが大きくなる問題を修正

--- cgi-bin/gcode2mcu.py
+++ cgi-bin/gcode2mcu.py
@@ -6,13 +6,13 @@ class McuConnection(object):
     	tn = telnetlib.Telnet('localhost', 6571)
         for line in gcodes:
             str = line.strip()
-            print 'W ' + str
+            #print 'W ' + str
             tn.write(str + '\n')
             buf = ''
 	    while buf == '':
                 buf = tn.read_until('> ') # ready?
-                print 'R "' + buf + '"'
-                sys.stdout.flush()
+                #print 'R "' + buf + '"'
+                #sys.stdout.flush()
         tn.close()
 
 if __name__ == "__main__":
